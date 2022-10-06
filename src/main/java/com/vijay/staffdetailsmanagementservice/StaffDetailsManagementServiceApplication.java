ca841c527e4dd0773c820a72ee42d7666a61c74c
vijayg21 <vijaygangadharam@gmail.com>
2022-10-06 15:55:36 +0530

    Initial commit

---
+++ src/main/java/com/vijay/staffdetailsmanagementservice/StaffDetailsManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vijay.staffdetailsmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffDetailsManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffDetailsManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vijay/staffdetailsmanagementservice/StaffDetailsManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijay.staffdetailsmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffDetailsManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
